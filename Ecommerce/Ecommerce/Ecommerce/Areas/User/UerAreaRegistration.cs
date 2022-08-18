640a7e0730c865ab8328d5e521b9c4ba60b96fe0
Akshay <driveakshay5@gmail.com>
2022-08-18 11:10:44 +0530

    Module4

---
+++ Ecommerce/Ecommerce/Ecommerce/Areas/User/UerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Ecommerce.Areas.Uer
+{
+    public class UerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Uer";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Uer_default",
+                "Uer/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
